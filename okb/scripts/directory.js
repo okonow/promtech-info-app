f581ecffcdc4a7367ac06e4dcf47e725c428225d
mu63n64 <159431223+mu63n64@users.noreply.github.com>
2025-05-12 16:34:31 +0300

    Update directory.js

--- okb/scripts/directory.js
+++ okb/scripts/directory.js
@@ -1,13 +1,13 @@
 function createContact(contactData) {
 	return `
-<div class=contact id=c${contactData.id}>${contactData.phone_number}</div>
+<div class=contact id=c${contactData.id}>${u.middle_name} ${u.first_name} ${u.last_name}</div>
 	`;
 }
 export async function showContacts(searchQuery) {
 	let oldContacts = document.querySelector('div.dir').querySelectorAll('div.contact');
 	for (let old of oldContacts) old.parentNode.removeChild(old);
 	
-	let contacts = await fetch('dbmu/contacts.json').then(res => res.json());
+	let contacts = await fetch('api/users').then(res => res.json());
 	let divDir = document.querySelector('div.dir');
 	for (let contact of contacts) { 
 		if (!searchQuery) {
